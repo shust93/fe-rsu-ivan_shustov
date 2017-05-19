b892a14eab3cec3c451007e09111eb7a280ca750
shust93 <ferdinandrig@gmail.com>
2017-05-19 17:19:45 +0300

    1

--- task3/script.js
+++ task3/script.js
@@ -2,17 +2,16 @@
 function consoleRec(arr, i) {
     if (i === undefined) {
         i = 0;
-    }	
+    }
     if (i < arr.length) {
         console.log(arr[i]);
         i += 1;
         consoleRec(arr, i);
     }
-    if (i = arr.length) {
+    if (i === arr.length) {
         i = 0;
     }
 }
-
 var array = ["я", "умею", "писать", "рекурсивные", "функции"];
 consoleRec(array);
 consoleRec(array);
